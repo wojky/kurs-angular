bd368528c584ea5d5cc85af54faa4dbbecf3f7b7
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-08-24 16:10:25 +0200

    feat: lesson 01 after class

--- 01-components-basics-and-template-syntax1/src/app/app.component.ts
+++ 01-components-basics-and-template-syntax1/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from "@angular/core";
+import { TasksListComponent } from "./tasks-list.component";
+import { SubmitTextComponent } from "./submit-text.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [TasksListComponent, SubmitTextComponent],
+
+  template: `
+    <h1 class="text-orange-500 bg-black py-4 text-xl text-center mb-4">
+      Another boring todolist
+    </h1>
+    <app-submit-text (submitText)="addTask($event)" />
+    <app-tasks-list [tasks]="tasks" />
+  `,
 })
 export class AppComponent {
-  title = "01-components-basics-and-template-syntax1";
+  tasks = [
+    {
+      name: "Angular introduction",
+      done: false,
+    },
+    {
+      name: "Learn components",
+      done: true,
+    },
+  ];
+
+  addTask(name: string) {
+    this.tasks.push({
+      name,
+      done: false,
+    });
+  }
 }
