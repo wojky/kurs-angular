06a416c5b4ba23ea0e63c5c2e0d811394853fe49
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-10-31 19:14:05 +0100

    prepare lesson 8 starter

---
+++ 08/src/app/task/data-access/tasks.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from "@angular/core";
+import { Task } from "../model/Task";
+import { ListFetchingError } from "../../utils/list-state.type";
+import { wait } from "../../utils/wait";
+
+export type TaskUpdatePayload = { done?: boolean; name?: string };
+
+export type GetAllTasksSearchParams = {
+  q: string;
+  _sort: "createdAt";
+  _order: "desc" | "asc";
+  done_like: "true" | "false" | "";
+};
+
+@Injectable({
+  providedIn: "root",
+})
+export class TasksService {
+  private URL = "http://localhost:3000";
+
+  async getAll(searchParams: GetAllTasksSearchParams) {
+    await wait();
+
+    const url = new URL("/tasks", this.URL);
+
+    url.search = new URLSearchParams(searchParams).toString();
+
+    return fetch(url).then<Task[] | ListFetchingError>((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return { status: response.status, message: response.statusText };
+    });
+  }
+
+  async delete(taskId: number) {
+    return fetch(`${this.URL}/tasks/${taskId}`, {
+      method: "DELETE",
+    }).then<Error | undefined>((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return new Error("Cant delete task");
+    });
+  }
+
+  async update(taskId: number, payload: TaskUpdatePayload) {
+    return fetch(`${this.URL}/tasks/${taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    }).then<Task | Error>((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return new Error("Cant update task");
+    });
+  }
+
+  async add(name: string) {
+    await wait();
+
+    return fetch(`${this.URL}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        done: false,
+      } as Task),
+    }).then<Task | Error>((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return new Error("Cant add task");
+    });
+  }
+}
---
+++ 08/src/app/task/task-list.page.component.ts
@@ -0,0 +1,87 @@
+import { Component, inject } from "@angular/core";
+import { TasksListComponent } from "./ui/tasks-list.component";
+import { Task } from "./model/Task";
+import { NgIf } from "@angular/common";
+import { GetAllTasksSearchParams, TasksService } from "./data-access/tasks.service";
+import { ComponentListState, LIST_STATE_VALUE } from "../utils/list-state.type";
+import { SubmitTextComponent } from "@ui/submit-text.component";
+import {
+  TasksListFiltersComponent,
+  TasksListFiltersFormValue,
+} from "./ui/task-list-filters.component";
+
+@Component({
+  selector: "app-task-list-page",
+  standalone: true,
+  imports: [TasksListComponent, SubmitTextComponent, NgIf, TasksListFiltersComponent],
+  template: `
+    <app-submit-text
+      (submitText)="
+        listState.state === listStateValue.SUCCESS && addTask($event, listState.results)
+      "
+    />
+    <app-tasks-list-filters (filtersChange)="handleFiltersChange($event)" />
+    <app-tasks-list
+      *ngIf="listState.state === listStateValue.SUCCESS"
+      class="block mt-4"
+      [tasks]="listState.results"
+    />
+    <p *ngIf="listState.state === listStateValue.ERROR">
+      {{ listState.error.message }}
+    </p>
+    <p *ngIf="listState.state === listStateValue.LOADING">Loading...</p>
+  `,
+})
+export class TaskListPageComponent {
+  private tasksService = inject(TasksService);
+
+  listState: ComponentListState<Task> = { state: LIST_STATE_VALUE.IDLE };
+  listStateValue = LIST_STATE_VALUE;
+
+  ngOnInit() {
+    // this.getAllTasks(getAllTasksSearchParams(this.form.getRawValue()));
+  }
+
+  handleFiltersChange(filters: TasksListFiltersFormValue): void {
+    this.getAllTasks(getAllTasksSearchParams(filters));
+  }
+
+  getAllTasks(searchParams: GetAllTasksSearchParams): void {
+    this.listState = { state: LIST_STATE_VALUE.LOADING };
+
+    this.tasksService.getAll(searchParams).then((response) => {
+      console.log({ response });
+      if (Array.isArray(response)) {
+        this.listState = {
+          state: LIST_STATE_VALUE.SUCCESS,
+          results: response,
+        };
+      } else {
+        this.listState = {
+          state: LIST_STATE_VALUE.ERROR,
+          error: response,
+        };
+      }
+    });
+  }
+
+  addTask(name: string, tasks: Task[]): void {
+    this.tasksService.add(name).then((response) => {
+      if ("id" in response) {
+        this.listState = {
+          state: LIST_STATE_VALUE.SUCCESS,
+          results: tasks.concat(response),
+        };
+      } else {
+        alert(response.message);
+      }
+    });
+  }
+}
+function getAllTasksSearchParams(filters: {
+  searchTerm: string;
+  status: "ALL" | "TODO" | "DONE";
+  sortBy: "ASC" | "DESC";
+}): GetAllTasksSearchParams {
+  throw new Error("Function not implemented.");
+}
