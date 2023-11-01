580ca36ef9dfffabf83ea908cccb9c82e8e68655
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-11-01 16:24:50 +0100

    rename lesson 8 directory

--- 08-rxjs-intro/src/app/task/ui/task-list-filters.component.ts
+++ 08-rxjs-intro/src/app/task/ui/task-list-filters.component.ts
@@ -8,7 +8,7 @@ import {
 import { SortBy, SORT_BY } from "src/app/shared/enums/sort-by.enum";
 import { TaskStatus, TASK_STATUS } from "../model/task-status.enum";
 import { FormValue } from "src/app/utils/form-value.type";
-import { startWith } from "rxjs";
+import { Subscription, debounceTime, startWith } from "rxjs";
 
 type TasksListFiltersForm = FormGroup<{
   searchTerm: FormControl<string>;
@@ -112,6 +112,8 @@ export class TasksListFiltersComponent {
   protected sortOptions = SORT_BY;
   protected statusOptions = TASK_STATUS;
 
+  private formChangesSubscription?: Subscription;
+
   sort(sort: SortBy) {
     this.form.patchValue({
       sortBy: sort,
@@ -119,8 +121,14 @@ export class TasksListFiltersComponent {
   }
 
   ngOnInit() {
-    this.form.valueChanges.pipe().subscribe(() => {
-      this.filtersChange.emit(this.form.getRawValue());
-    });
+    this.formChangesSubscription = this.form.valueChanges
+      .pipe(startWith(this.form.value), debounceTime(200))
+      .subscribe((v) => {
+        this.filtersChange.emit(this.form.getRawValue());
+      });
+  }
+
+  ngOnDestroy() {
+    this.formChangesSubscription?.unsubscribe();
   }
 }
