74eb148eb0a40b2d6a5a69df432f3dd15b7a24f2
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-11-18 08:17:10 +0100

    feat: add projects feature

--- 10-routing/src/app/app.component.ts
+++ 10-routing/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from "@angular/core";
 import { TaskListPageComponent } from "./task/task-list.page.component";
+import { ProjectListPageComponent } from "./project/project-list.page.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [TaskListPageComponent],
+  imports: [TaskListPageComponent, ProjectListPageComponent],
 
   template: `
     <h1 class="text-orange-500 uppercase py-4 text-2xl text-center">
       Another boring todolist
     </h1>
-    <main class="grid place-items-center pt-4">
+    <main class="grid pt-4 px-12">
+      <app-project-list-page />
+      <hr class="my-6" />
       <app-task-list-page />
     </main>
   `,
