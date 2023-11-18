6ff981f3e2a691fb5ba988e9d1637b6e5cda5e37
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-11-18 11:53:32 +0100

    feat: router, router links

--- 10-routing/src/app/app.component.ts
+++ 10-routing/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from "@angular/core";
 import { TaskListPageComponent } from "./task/task-list.page.component";
 import { ProjectListPageComponent } from "./project/project-list.page.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [TaskListPageComponent, ProjectListPageComponent],
+  imports: [
+    TaskListPageComponent,
+    ProjectListPageComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   styles: [
     `
       main,
@@ -18,16 +25,14 @@ import { ProjectListPageComponent } from "./project/project-list.page.component"
     <h1 class="text-orange-500 uppercase py-4 text-2xl text-center">
       Another boring todolist
     </h1>
-    <!-- <nav class="bg-orange-300">
+    <nav class="bg-orange-300">
       <ul class="flex gap-6">
-        <li><a>Tasks</a></li>
-        <li><a>Projects</a></li>
+        <li><a routerLink="/tasks" routerLinkActive="font-bold">Tasks</a></li>
+        <li><a routerLink="/projects" routerLinkActive="font-bold">Projects</a></li>
       </ul>
-    </nav> -->
+    </nav>
     <main class="grid pt-4">
-      <app-project-list-page />
-      <hr class="my-6" />
-      <app-task-list-page />
+      <router-outlet />
     </main>
   `,
 })
--- 10-routing/src/app/app.config.ts
+++ 10-routing/src/app/app.config.ts
@@ -1,6 +1,29 @@
 import { ApplicationConfig } from "@angular/core";
 import { provideHttpClient } from "@angular/common/http";
+import { Routes, provideRouter } from "@angular/router";
+import { ProjectListPageComponent } from "./project/project-list.page.component";
+import { TaskListPageComponent } from "./task/task-list.page.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "tasks",
+    pathMatch: "full",
+  },
+  {
+    path: "projects",
+    component: ProjectListPageComponent,
+  },
+  {
+    path: "tasks",
+    component: TaskListPageComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "tasks",
+  },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(), provideRouter(routes)],
 };
