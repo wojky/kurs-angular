fd27e516f997d2a1e5988a28b515872e14d1f258
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2024-02-09 19:54:15 +0100

    feat: lesson 14 starter

---
+++ 14-services-responsibility-decoupling/src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideHttpClient(), provideRouter(routes, withComponentInputBinding())],
+};
